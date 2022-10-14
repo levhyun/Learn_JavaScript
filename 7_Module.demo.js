a6cae636aaefc025fbb4d068465a24a4401b9203
levhyun <g88080997@gmail.com>
2022-10-14 10:44:05 +0900

    너무 힘들다아아아......ㅠ

--- 7_Module.demo.js
+++ 7_Module.demo.js
@@ -5,6 +5,14 @@ var circle = require("./8_Module.circle.js");
 // 8_Module.circle.js 파일을 읽어와 변수 circle 대입. (require = 읽어 오는 명령어)
 console.log("the area of a circle of radius 4 is " + circle.area(4)); // circle변수에 area함수호출.
 
+/*
+실행결과
+    [Running] node "c:\Users\user\Documents\GitHub\Learn_JavaScript\7_Module.demo.js"
+    the area of a circle of radius 4 is 50.26548245743669
+
+    [Done] exited with code=0 in 0.204 seconds
+*/
+
 /* 
     라이브러리란?
         JavaScript 라이브러리는 웹 개발자가 표준 JavaScript 함수를 실행하기 위해 재사용할 수 있는 미리 작성된 코드 조각의 모음입니다.
---
+++ 9_Regular_expression.js
@@ -0,0 +1,36 @@
+// 정규표현식
+
+// pattern
+var pattern = /a/; // pattern에 a 저장
+
+var pattern2 = new RegExp("a"); // pattern2에 a 저장
+
+const log = console.log;
+
+log(pattern.exec("abcdef")); // "abcdef"에서 pattern찾기
+log(pattern2.exec("abcdef")); // "abcdef"에서 pattern2찾기
+
+log(pattern.test("assef")); // True
+log(pattern2.test("bsdg")); // False
+
+// String
+log("abcdef".match(pattern));
+log("jbcdef".match(pattern2)); // null
+
+log("abcdef".replace(pattern), "A"); // pattern을 찾고 "A"로 바꾼다.
+
+// 옵션
+var xi = /a/i; // i를 붙이면 대소문자를 구분하지 않는다.
+log("Abcde".match(xi)); // ["A"]
+
+var xg = /a/g;
+log("abcda".match(xg)); // pattern값을 검색한 모든 결과값을 리턴.
+
+var ig = /a/gi;
+log("AbDaGaA".match(ig)); // g + i
+
+// 캡쳐
+var pattern = /(\w+)\s(\w+)/;
+var str = "coding everybody";
+var result = str.replace(re, "$2, $1");
+log(result);
